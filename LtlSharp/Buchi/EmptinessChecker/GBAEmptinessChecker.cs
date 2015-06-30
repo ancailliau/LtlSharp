46ee9ddea15ba4ab6c7efd9345384d6bd49e71e0
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2015-06-30 10:52:08 +0200

    Fixes GBA emptiness checker

--- LtlSharp/Buchi/EmptinessChecker/GBAEmptinessChecker.cs
+++ LtlSharp/Buchi/EmptinessChecker/GBAEmptinessChecker.cs
@@ -31,9 +31,11 @@ namespace LtlSharp.Buchi
             label = new HashSet<int>[a.Nodes.Length];
             
             foreach (var n in a.Nodes.Where (x => x.Initial)) {
+                Console.WriteLine ("******");
                 if (EmptinessSearch (a, n)) {
                     return true;
                 }
+                Console.WriteLine ("******");
             }
             
             return false;
@@ -49,36 +51,53 @@ namespace LtlSharp.Buchi
             path.Push (qi.Id);
             while (path.Count > 0) {
                 var q = path.Peek ();
+                Console.WriteLine ("q={0}", a.Nodes[q].Name);
+            
                 var succToProcess = a.Transitions[q].Select (x => x.To).Except (path.Union (processed));
                 while (succToProcess.Count () > 0) {
                     var succ = succToProcess.First ();
                     path.Push (succ);
                     label [succ] = new HashSet<int> ();
                     q = succ;
+                    succToProcess = a.Transitions[q].Select (x => x.To).Except (path.Union (processed));
+                    Console.WriteLine ("q={0}", a.Nodes[q].Name);
                 }
+                Console.WriteLine ("----");
                 if (label[q].Count == 0 | a.AcceptanceSets.Any (x => x.Nodes.Contains (q))) {
                     var labelsToPropagate = label [q].Union ((from x in a.AcceptanceSets
                                                                              where x.Nodes.Contains (q)
                                                                              select x.Id));
+                    Console.WriteLine ("labelsToPropagate={0}", string.Join (",", labelsToPropagate));
+            
                     propagate (a, new [] { q }, labelsToPropagate);
                     if (label[q].SetEquals (a.AcceptanceSets.Select (set => set.Id))) {
-                        return true;
+                        goto ExitTrue;
+                        // return true;
                     }
                 }
                 processed.Add (q);
                 path.Pop ();
             }
+
             return false;
+            
+            ExitTrue : return true;
         }
         
 
         void propagate (GeneralizedBuchiAutomata a, IEnumerable<int> nodes, IEnumerable<int> labelsToPropagate)
         {
+            
+            Console.WriteLine ("path : " + string.Join(",", path));
+            Console.WriteLine ("processed : " + string.Join(",", processed));
+            
             var toProp = labelsToPropagate.ToArray ();
             Stack<int> nodesToProcess = new Stack<int> (nodes);
             while (nodesToProcess.Count > 0) {
                 var q = nodesToProcess.Pop ();
-                var successors = a.Transitions [q].Select (x => x.To).Except (path).Except (processed).ToArray ();
+                var successors = a.Transitions [q].Select (x => x.To).Intersect (path.Union (processed)).ToArray ();
+                Console.WriteLine ("successors : " + string.Join(",", a.Transitions [q].Select (x => a.Nodes[x.To].Name)));
+                Console.WriteLine ("successors : " + string.Join(",", successors));
                 foreach (var succ in successors) {
                     if (!label[succ].IsSupersetOf (labelsToPropagate)) {
                         nodesToProcess.Push (succ);
