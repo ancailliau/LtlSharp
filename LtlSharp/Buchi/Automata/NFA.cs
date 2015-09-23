4002616bc346788fef93ed811007e2dceedc6819
Antoine Cailliau <antoine.cailliau@uclouvain.be>
2015-09-23 11:45:12 +0200

    Implements NFA and their translation from BA

--- LtlSharp/Buchi/Automata/NFA.cs
+++ LtlSharp/Buchi/Automata/NFA.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace LtlSharp.Buchi.Automata
 {
@@ -10,8 +13,44 @@ namespace LtlSharp.Buchi.Automata
     /// </description>
     public class NFA
     {
-        public NFA ()
+        public AutomataNode [] Nodes { get; set; }
+        public List<AutomataTransition>[] Transitions { get; set; }
+        public int[] AcceptanceSet { get; set; }
+
+        public NFA (int n_nodes)
         {
+            Nodes = new AutomataNode[n_nodes];
+            Transitions = new List<AutomataTransition>[n_nodes];
+            AcceptanceSet = new int[0];
+        }
+
+        public string ToDot () 
+        {
+            var str = new StringWriter ();
+            var dict = new Dictionary<AutomataNode, string> ();
+            int i = 0;
+            str.WriteLine ("digraph G {");
+            foreach (var n in Nodes.Where (x => x != null)) {
+                dict.Add (n, "s" + (i++));
+                str.WriteLine ("\t" + dict[n] + "[label=\""+n.Name+"\""
+                               +(AcceptanceSet.Contains(n.Id) ? ",shape=doublecircle" : "")
+                               +(n.Initial ? ",penwidth=3" : "")+
+                               "];");
+            } 
+            for (int j = 0; j < Transitions.Length; j++) {
+                var node = Transitions [j];
+                if (node == null)
+                    continue;
+                foreach (var t in node.Where (x => x != null)) {
+                    if (Nodes[j] == null | Nodes[t.To] == null) {
+                        continue;
+                    }
+                    str.WriteLine ("\t" + dict [Nodes [j]] + " -> " + dict [Nodes [t.To]] + " [label=\"" + string.Join (",", t.Labels) + "\"];");
+                }
+            }
+            str.WriteLine ("}");
+
+            return str.ToString ();
         }
     }
 }
--- LtlSharp/Buchi/Translators/BA2NFA.cs
+++ LtlSharp/Buchi/Translators/BA2NFA.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using LittleSharp.Buchi;
+using LtlSharp.Buchi.Automata;
+
 namespace LtlSharp.Buchi.Translators
 {
-    public class BA2NFA
+    public static class BA2NFA
     {
-        public BA2NFA ()
+        public static NFA Translate (BuchiAutomata automata)
         {
+            var emptinessChecker = new EmptinessChecker (automata);
+            
+            var newAcceptanceSet = new List<int> ();
+            foreach (var n in automata.Nodes) {
+                if (emptinessChecker.Emptiness (n))
+                    newAcceptanceSet.Add (n.Id);
+            }
+
+            var nfa = new NFA (0);
+            nfa.Nodes = automata.Nodes;
+            nfa.Transitions = automata.Transitions;
+            nfa.AcceptanceSet = newAcceptanceSet.ToArray ();
+            return nfa;
         }
     }
 }
